70e152ebfa181161e21fa9b7bbea584a5f995054
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-22 22:10:07 +0200

    Update CustomActions.js
    add actionsheet

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,8 +1,15 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { connectActionSheet } from "@expo/react-native-action-sheet";
+
 //let user to select photo from their library or take a new photo
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 //let user to send a location
 import * as Location from "expo-location";
+
+
 
 
 class CustomActions extends React.Component {
@@ -65,3 +72,71 @@ class CustomActions extends React.Component {
       console.log(error.message);
     }
   };
+
+  // ACTION SHEET 
+  onActionPress = () => {
+    const options = [
+      "Choose From Library",
+      "Take Picture",
+      "Send Location",
+      "Cancel",
+    ];
+    const cancelButtonIndex = options.length - 1;
+    this.props.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            console.log("user wants to pick an image");
+            this.pickImage();
+            break;
+          case 1:
+            console.log("user wants to take a photo");
+            this.takePhoto();
+            break;
+          case 2:
+            console.log("user wants to get their location");
+            this.getLocation();
+          default:
+            break;
+        }
+      }
+    );
+  };
+
+
+  render() {
+    return (
+      <TouchableOpacity style={[styles.container]} onPress={this.onActionPress}>
+        <View style={[styles.wrapper, this.props.wrapperStyle]}>
+          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+//StyleSheet
+  const styles = StyleSheet.create({
+    container: {
+      width: 26,
+      height: 26,
+      marginLeft: 10,
+      marginBottom: 10,
+    },
+    wrapper: {
+      borderRadius: 13,
+      borderColor: "#b2b2b2",
+      borderWidth: 2,
+      flex: 1,
+    },
+    iconText: {
+      color: "#b2b2b2",
+      fontWeight: "bold",
+      fontSize: 16,
+      backgroundColor: "transparent",
+      textAlign: "center",
+    },
+  });
